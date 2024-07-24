a08d9cf6a27564685361f286980e50241cc63b5a
Deven Amador <amadordeven10@gmail.com>
2024-07-24 11:23:39 -0700

    Initial Commit

---
+++ FitData/FitDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitDataApp.swift
+//  FitData
+//
+//  Created by Deven Amador on 7/24/24.
+//
+
+import SwiftUI
+
+@main
+struct FitDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
